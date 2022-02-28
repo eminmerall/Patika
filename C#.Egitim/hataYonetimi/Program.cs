b07f6d87d369b9301fa0b17a22e9fc66834e11cf
Emin Meral <eminmerall@gmail.com>
2022-02-28 20:51:24 +0300

    Hata Yönetimi

---
+++ C#.Egitim/hataYonetimi/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace hataYonetimi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+         {
+             Console.WriteLine("Bir sayı giriniz :");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Girdiğiniz sayı : " + a);
+         }
+
+         catch(Exception ex)
+         {
+           Console.WriteLine(ex.Message);
+         }
+         finally
+         {
+             Console.WriteLine("İşlem Tamalandı");
+             Console.ReadKey();
+         }                   
+        }
+    }
+}
