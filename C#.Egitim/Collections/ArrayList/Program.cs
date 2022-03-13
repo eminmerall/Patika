f075130f8d8d9d0d3d9bf52eee11c0fbe194e517
Emin Meral <eminmerall@gmail.com>
2022-03-13 14:02:32 +0300

    koleksiyonlar

---
+++ C#.Egitim/Collections/ArrayList/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+namespace arrayList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList liste = new ArrayList();
+            //listeye ekleme
+            liste.Add("Ayşe");
+            liste.Add(2);
+            liste.Add(true);
+            liste.Add("A");
+
+            //verilere erişme
+
+            //Console.WriteLine(liste[1]);
+
+            foreach (var item in liste)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("--------");
+            //listeye aynı anda birden fazla ekleme ekleme
+
+            //string[] renkler = {"Kırmızı","Sarı","Yeşil"};
+            ArrayList liste2 = new ArrayList();
+
+            List<int> sayilar = new List<int>(){1,12,6,9,5,0,15,8,4,2,5}; 
+            //liste.AddRange(renkler);
+            liste2.AddRange(sayilar);
+
+            foreach (var item in liste2)
+            {
+                Console.WriteLine(item);
+            }
+            //sıralama
+
+            Console.WriteLine("----------");
+            liste2.Sort();
+            foreach (var item in liste2)
+            {
+                Console.WriteLine(item);
+            }
+            //Binary Search (sıralama yapmak gerekir)
+            Console.WriteLine("----------");
+            Console.WriteLine(liste2.BinarySearch(15));
+            Console.WriteLine("----------");
+
+            liste2.Reverse();
+            foreach (var item in liste2)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
