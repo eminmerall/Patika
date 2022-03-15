a95a39d890500377ad5417be6f4a1c2b97cd470c
Emin Meral <eminmerall@gmail.com>
2022-03-15 15:50:35 +0300

    ödev3

---
+++ C#.Egitim/odev2/Koleksiyonlar-Soru-3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Koleksiyonlar_Soru_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>();
+            List<string> myList = new List<string>();
+
+            string[] sesli = { "a","e","i","ı","o","ö","u","ü" };
+
+            Console.WriteLine("Bir cümle giriniz :");
+
+            myList.Add(Console.ReadLine());
+
+ 
+            for (int i = 0; i < myList.Count; i++)
+                for(int j = 0; j < sesli.Length; j++)
+                {
+                    if (myList[i].Contains(sesli[j]))
+                    {
+                       list.Add(sesli[j]);
+                    }
+                }           
+            list.ForEach(item => Console.WriteLine(item));
+            Console.ReadLine();
+        }
+    }
+}
