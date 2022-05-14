78f95c7cb05e102bde4909d6c588664641ee650c
Emin Meral <eminmerall@gmail.com>
2022-05-14 21:11:59 +0300

    Struct(Yapı) Kavramı

---
+++ C#.Egitim/Class/class5/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace class5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Diktortgen diktortgen = new Diktortgen();
+            diktortgen.KisaKenar = 10;
+            diktortgen.UzunKenar= 22;
+            Console.WriteLine("Class alan hesabı :{0}",diktortgen.AlanHesapla());
+
+            Diktortgen_struct diktortgen_struct; // = new Diktortgen_struct();
+            diktortgen_struct.KisaKenar = 5;
+            diktortgen_struct.UzunKenar= 18;
+            Console.WriteLine("Struct alan hesabı :{0}",diktortgen_struct.AlanHesapla());
+
+            Console.ReadLine();
+        }
+    }
+
+    class Diktortgen
+    {
+        public int KisaKenar;
+        public int UzunKenar;
+
+        public long AlanHesapla()
+        {
+            return this.KisaKenar * this.UzunKenar;
+        }
+    }
+
+    struct Diktortgen_struct
+    {
+        public int KisaKenar;
+        public int UzunKenar;
+
+        public long AlanHesapla()
+        {
+            return this.KisaKenar * this.UzunKenar;
+        }
+    }
+}
